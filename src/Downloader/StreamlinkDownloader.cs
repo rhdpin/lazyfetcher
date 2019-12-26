f40dbe67d8112c150b306e8e0c2d77547d08019a
rhdpin <rhdpin@protonmail.com>
2019-12-26 13:08:04 +0200

    Output debug log from Streamlink when in verbose mode

--- src/Downloader/StreamlinkDownloader.cs
+++ src/Downloader/StreamlinkDownloader.cs
@@ -68,11 +68,16 @@ namespace LazyFetcher.Downloader
                 proxyString = $"--https-proxy https://127.0.0.1:{_proxy.Port}";
             }
 
+            var loggingString = String.Empty;
+            if (_options.VerboseMode)
+            {
+                loggingString = $"-v -l debug";
+            }
             var streamUrl = request.StreamUrl.Replace("https://", "http://");                    
             
             var streamArgs = $"\"hlsvariant://{streamUrl} name_key=bitrate verify=False\" {_options.Bitrate} --http-header " +
                                 $"\"User-Agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) " +
-                                $"Chrome/59.0.3071.115 Safari/537.36\" --hls-segment-threads=4 {proxyString} -f -o {request.TargetFileName}";
+                                $"Chrome/59.0.3071.115 Safari/537.36\" --hls-segment-threads=4 {proxyString} {loggingString} -f -l debug -v -o {request.TargetFileName}";
 
             _messenger.WriteLine($"Starting download with command '{StreamLinkAppName} {streamArgs}", Messenger.MessageCategory.Verbose);
 
@@ -96,65 +101,78 @@ namespace LazyFetcher.Downloader
                 _messenger.WriteLine($"Could not start downloader '{StreamLinkAppName}': {e.Message}. Ensure that the downloader application is located in current directory or $PATH.");
                 return;
             }            
-                        
-            Task.Run(() =>
-            {
-                var counter = 1;
-                long previousSize = 0;                                
-                DateTime previousTime = DateTime.Now;
-                double downloadRate = 0;
-                string downloadRateString;
 
-                while (!_process.HasExited)
+            if (!_options.VerboseMode)                        
+            {
+                Task.Run(() =>
                 {
-                    Thread.Sleep(500);
-                    
-                    if (File.Exists(request.TargetFileName))
+                    var counter = 1;
+                    long previousSize = 0;                                
+                    DateTime previousTime = DateTime.Now;
+                    double downloadRate = 0;
+                    string downloadRateString;
+
+                    while (!_process.HasExited)
                     {
-                        var currentSize = new FileInfo(request.TargetFileName).Length;
-                        if (counter % 10 == 0)
-                        {                            
-                            var currentTime = DateTime.Now;                            
-                            var secondsElapsed = (currentTime - previousTime).TotalMilliseconds / (double) 1000;
-                            var bytesDownloaded = currentSize - previousSize;
-                            downloadRate = (bytesDownloaded / 1024 / 1024 / secondsElapsed);                            
-                            previousSize = currentSize;
-                            previousTime = currentTime;
+                        Thread.Sleep(500);
+                        
+                        if (File.Exists(request.TargetFileName))
+                        {
+                            var currentSize = new FileInfo(request.TargetFileName).Length;
+                            if (counter % 10 == 0)
+                            {                            
+                                var currentTime = DateTime.Now;                            
+                                var secondsElapsed = (currentTime - previousTime).TotalMilliseconds / (double) 1000;
+                                var bytesDownloaded = currentSize - previousSize;
+                                downloadRate = (bytesDownloaded / 1024 / 1024 / secondsElapsed);                            
+                                previousSize = currentSize;
+                                previousTime = currentTime;
+                            }
+                            downloadRateString = (downloadRate > 0) ? downloadRate.ToString("0.0") : "---";                        
+
+                            _messenger.OverwriteLine($"Writing stream to file: {currentSize / 1024 / 1024} MB ({downloadRateString} MB/s)");
+                            counter++;
                         }
-                        downloadRateString = (downloadRate > 0) ? downloadRate.ToString("0.0") : "---";                        
-
-                        _messenger.OverwriteLine($"Writing stream to file: {currentSize / 1024 / 1024} MB ({downloadRateString} MB/s)");
-                        counter++;
                     }
-                }
-            });
+                });
+                
+                _process.WaitForExit();
+                _messenger.WriteLine("");
 
-            _process.WaitForExit();
-            _messenger.WriteLine("");
-
-            var unexpectedOutput = false;
-            // Write Streamlink output to console (other than the regular messages)
-            while (!_process.StandardOutput.EndOfStream)
-            {
-                var line = _process.StandardOutput.ReadLine();
-
-                if (line != null && !line.StartsWith("[cli][info]") && !line.Contains("[download]"))
+                var unexpectedOutput = false;
+                
+                // Write Streamlink output to console (other than the regular messages)
+                while (!_process.StandardOutput.EndOfStream)
                 {
-                    _messenger.WriteLine($"{line}");
-                    unexpectedOutput = true;
-                }
-                else if (_options.VerboseMode)
+                    var line = _process.StandardOutput.ReadLine();
+
+                    if (line != null && !line.StartsWith("[cli][info]") && !line.Contains("[download]"))
+                    {
+                        _messenger.WriteLine($"{line}");
+                        unexpectedOutput = true;
+                    }
+                    else if (_options.VerboseMode)
+                    {
+                        _messenger.WriteLine($"{line}");
+                    }
+                }  
+
+                if (unexpectedOutput)
                 {
-                    _messenger.WriteLine($"{line}");
-                }
-            }  
-            
-            if (unexpectedOutput)
-            {
-                _messenger.WriteLine("\nLooks like something went wrong. Please check that redirection is configured either by editing " +
-                    "hosts file or by using proxy (parameter '-x' and requires that mlbamproxy is found). " +
-                    "By default application expects that hosts file is edited");
+                    _messenger.WriteLine("\nLooks like something went wrong. Please check that redirection is configured either by editing " +
+                        "hosts file or by using proxy (parameter '-x' and requires that mlbamproxy is found). " +
+                        "By default application expects that hosts file is edited");            
+                }            
             }
+            else
+            {
+                // Output all output from Streamlink
+                while (!_process.StandardOutput.EndOfStream)
+                {
+                    Thread.Sleep(200);
+                    Console.WriteLine(_process.StandardOutput.ReadLine());
+                }
+            }             
         }
     }
 }
